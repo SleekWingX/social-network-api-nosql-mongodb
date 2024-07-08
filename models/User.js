bfc39ad74744d5c8b06be39457ba7d8dc2e7b0c3
SleekWingX <alexisyazzie365@gmail.com>
2024-07-08 16:34:04 -0600

    initial commit

---
+++ models/User.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+
+//Schema to create User model
+const userSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trimmed: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, 'Must match a valid email address']
+  },
+  thoughts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Thought'
+    }
+  ],
+  friends: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    }
+  ]
+},
+{
+  toJSON: {
+    virtuals: true,
+  },
+  id: false
+});
+
+userSchema.virtual('friendCount').get(function() {
+  return this.friends.length;
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
